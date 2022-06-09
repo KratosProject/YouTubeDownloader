51afc100be64ee961d116228988f365649dedea8
KratosProject <86033620+KratosProject@users.noreply.github.com>
2022-06-09 11:42:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import  os
 
 BOT_TOKEN = os.environ.get("5425412029:AAF0w7xKZ_2oRkP7PsdARn7Qpkb28X79_JI")
-APP_ID = int(os.environ.get(7917796))
+APP_ID = os.environ.get(7917796)
 API_HASH = os.environ.get("ce7aec4e1c0a3f0edf83396ab5d58e94")
 
 youtube_next_fetch = 0  # time in minute
