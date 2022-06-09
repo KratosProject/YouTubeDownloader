1ccb3b07317b262bc96a67a9fc1768864948bc07
KratosProject <86033620+KratosProject@users.noreply.github.com>
2022-06-09 11:22:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("5425412029:AAF0w7xKZ_2oRkP7PsdARn7Qpkb28X79_JI")
 APP_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 
